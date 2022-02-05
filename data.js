452a908d9d25afdace8dcab30f8a8c46cd0196d9
Boris Krasko <boris.krasko@live.com>
2022-02-05 14:23:54 +0100

    feat: add data.js

---
+++ data.js
@@ -0,0 +1,83 @@
+const navigation  = {
+  nl: ['Home', 'Over ons', 'Activiteiten', 'Referenties', 'Nieuws', 'Vacatures', 'Contact',],
+  en: ['Home', 'About us', 'Activities', 'References', 'News', 'Vacancies', 'Contact',],
+  fr: ['Accueil', 'Qui Sommes-nous ?', 'Activités', 'Références', 'Nouvelles', "Offres D'emploi", 'Le contact',],
+  de: ['Startseite', 'Über uns', 'Aktivitäten', 'Referenzen', 'Aktuelles', 'Stellenangebote', 'Kontakt',],
+};
+
+const activities = {
+  nl: ['Vloerder', 'Schilderwerken', 'Gyprocwerken', 'Pleisterwerken', 'Terrassen en opritten', 'Dakwerken', 'Crepi', 'Renovatie', 'Nieuwbouw', 'Alle dossiers...',],
+  en: ['Floor', 'Painting', 'Gyproc works', 'Plastering', 'Terraces and driveways', 'Roofing works', 'Facades', 'Renovation', 'New construction', 'All...',],
+  fr: ['Revêtement de sol', 'Peintures', 'Travaux de gyprok', 'Plâtrage', 'Terrasses et allées', 'Travaux de toiture', 'Façades', 'Rénovation', 'Nouvelle construction', 'Tous les fichiers...',],
+  de: ['Etage', 'Malerei', 'Gyproc-Arbeiten', 'Verputzen', 'Terrassen und Einfahrten', 'Dachwerk', 'Crepi', 'Renovierung', 'Neubau', 'Alle Dateien...',],
+};
+
+const floor = {
+  nl:['Natuursteen', 'Keramiek', 'Mozaïek', 'Exclusieve tegels', 'Parket', 'Laminaat', 'Linoleum', 'Vinyl', 'Tapijt', 'PVC', 'LVT', 'Rubber', 'Egaliseren', ], 
+  en:['Natural stone', 'Ceramics', 'Mosaic', 'Exclusive tiles', 'Parquet', 'Laminate', 'Linoleum', 'Vinyl', 'Carpet', 'PVC', 'LVT', 'Rubber ', 'Equalize', ], 
+  fr:['Pierre naturelle', 'Céramique', 'Mosaïque', 'Carreaux exclusifs', 'Parquet', 'Laminé', 'Linoléum', 'Vinyle', 'Moquette', 'PVC', 'LVT', ' Caoutchouc ', 'Égaliser', ],
+  de:['Naturstein', 'Keramik', 'Mosaik', 'Exklusive Fliesen', 'Parkett', 'Laminat', 'Linoleum', 'Vinyl', 'Teppich', 'PVC', 'LVT', ' Gummi ', 'Ausgleichen', ], 
+}
+
+const contact = {
+  nl: ['Contactformulier', 'Bel ons', 'Bedrijfsgegevens',],
+  en: ['Contact form', 'Call us', 'Company details',],
+  fr: ['Formulaire de contact', 'Appelez-nous', 'Coordonnées de l\'entreprise',],
+  de: ['Kontaktformular', 'Rufen Sie uns an', 'Firmendaten',],
+}
+
+const searchTitleContent = {
+  nl: 'Wat zoek je?',
+  en: 'What are you looking for?',
+  fr: 'Qu\'est-ce que tu cherches?',
+  de: 'Was suchen Sie?',
+}
+
+const partnersTitleContent = {
+  nl: 'Onze Partners',
+  en: 'Our Partners',
+  fr: 'Nos partenaires',
+  de: 'Unsere Partner',
+}
+
+const contactLinkContent = {
+  nl: 'Contact',
+  en: 'Contact',
+  fr: 'Contacter',
+  de: 'Kontakt',
+}
+
+const homePage = {
+  nl: 'Startpagina',
+  en: 'Home Page',
+  fr: 'Page d\'accueil',
+  de: 'Startseite',
+}
+
+const footnavTitles = {
+  nl: ['Hoofd', 'Activiteiten', 'Soziale Netzwerke', 'Gegevens',],
+  en: ['Main', 'Activities', 'Socials', 'Data',],
+  fr: ['Principal', 'Activités', 'Réseaux sociaux', 'Données',],
+  de: ['Haupt', 'Aktivitäten', 'Soziale Netzwerke', 'Daten',],
+}
+
+const legals = {
+  nl: ['Maatschappelijke Zetel', 'Rubriek Oprichting', 'Financiële Gegevens', 'Algemene Voorwaarden', 'Ontworpen door GitHub.com',],
+  en: ['Registered Office', 'Incorporation Section', 'Financial Data', 'Terms and Conditions', 'Designed by GitHub.com',],
+  fr: ['Siège social', 'Section d\'incorporation', 'Données financières', 'Termes et conditions', 'Conçu par GitHub.com',],
+  de: ['Eingetragener Sitz', 'Gründungsabschnitt', 'Finanzdaten', 'Allgemeine Geschäftsbedingungen', 'Entworfen von GitHub.com',],
+}
+
+const more = {
+  nl: 'meer',
+  en: 'more',
+  fr: 'plus',
+  de: 'mehr',
+}
+
+const widgetTitle = {
+  nl: 'Contacteer ons',
+  en: 'Contact us',
+  fr: 'Nous contacter',
+  de: 'Kontaktiere uns',
+}
