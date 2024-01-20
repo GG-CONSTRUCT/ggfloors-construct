e9f466834b6e5e43186792dd0f726ac4f43bc680
Boris Krasko <boris.krasko@live.com>
2024-01-20 19:42:34 +0100

    fix: instagram link

--- js/main.js
+++ js/main.js
@@ -311,7 +311,7 @@ const toPrint = get('.to-print');
 const socials = {
   facebook: 'GGFloorsConstructGG',
   linkedin: 'company/ggfloors-construct',
-  instagram: 'gg_floors_constructions',
+  instagram: 'ggfloorsconstruct',
 }
 
 class Social {
